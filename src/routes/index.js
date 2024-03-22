3547fa6d87fe177529cb1f6a370b016e0aec8830
Huu Tuan <nhtuan010302@gmail.com>
2024-03-22 15:17:31 +0700

    Add login functionality and styling

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import { Login } from '~/pages';
 
-const publicRouters = [{ path: '/login', component: Login }];
+const publicRouters = [{ path: 'login', component: Login }];
 
 // const privateRouters = [{ path: '/manage-user', component: ManageUser }];
 
