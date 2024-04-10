ae97d37ca815e73ebf832f4b4744747229f3dc74
Huu Tuan <nhtuan010302@gmail.com>
2024-04-10 10:56:23 +0700

    Project initiation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from "@/pages/admin/login";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">React App</h1>
+      <Login />
+    </>
+  );
+}
+
+export default App;
