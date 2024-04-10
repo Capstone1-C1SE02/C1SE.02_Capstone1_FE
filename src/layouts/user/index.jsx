2d4a279764bfd7e51861556c237f8ff75e4bd173
Huu Tuan <nhtuan010302@gmail.com>
2024-04-10 18:31:39 +0700

    Add sidebar component to user layout and home page

--- src/layouts/user/index.jsx
+++ src/layouts/user/index.jsx
@@ -1,11 +1,19 @@
 import Header from "@/components/user/Header";
+import SideBar from "@/components/user/SideBar";
 
 function DefaultLayoutUser({ children }) {
   return (
-    <div className="flex h-[100vh] justify-center bg-[--primaryBackgroundColor] p-[30px]">
+    <div className="flex min-h-[100vh] justify-center bg-[--primaryBackgroundColor] p-[30px]">
       <div className="flex w-[1170px] flex-col gap-[30px]">
         <Header />
-        {children}
+        <main className="flex min-h-min flex-col gap-[20px] rounded-[20px] bg-white p-[30px]">
+          <h1 className="text-[30px] font-bold">Tra cứu thông tin văn bằng</h1>
+          <div className="flex flex-grow items-center gap-[30px]">
+            <SideBar />
+            <div className="h-[100%] w-[3px] rounded-full bg-[--primaryBackgroundColor]" />
+            {children}
+          </div>
+        </main>
       </div>
     </div>
   );
--- src/pages/user/Home/index.jsx
+++ src/pages/user/Home/index.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import SideBar from "@/components/user/SideBar";
 import { Button } from "@/components/ui/button";
 
 function Home() {
@@ -22,99 +21,86 @@ function Home() {
   };
 
   return (
-    <main className="flex min-h-min flex-col gap-[20px] rounded-[20px] bg-white p-[30px]">
-      <h1 className="text-[30px] font-bold">Tra cứu thông tin văn bằng</h1>
-      <div className="flex flex-grow items-center gap-[30px]">
-        <SideBar />
-        <div className="h-[100%] w-[3px] rounded-full bg-[--primaryBackgroundColor]" />
-        <form
-          className="flex w-full flex-col items-center space-y-[20px] p-[10px]"
-          onSubmit={handleSubmit}
-          method="POST"
-        >
-          <div className="w-full space-y-[15px]">
-            <div className="flex items-center gap-[30px]">
-              <label
-                htmlFor="SoHieuVanBang"
-                className="w-[200px] text-right font-[500]"
-              >
-                Số hiệu Văn bằng
-              </label>
-              <input
-                type="text"
-                name="SoHieuVanBang"
-                id="SoHieuVanBang"
-                value={soHieuVanBang}
-                onChange={(e) => setSoHieuVanBang(e.target.value)}
-                className={inputClassName()}
-              />
-            </div>
-            <div className="flex items-center gap-[30px]">
-              <label
-                htmlFor="SoVaoSo"
-                className="w-[200px] text-right font-[500]"
-              >
-                Số vào sổ
-              </label>
-              <input
-                type="text"
-                name="SoVaoSo"
-                id="SoVaoSo"
-                value={soVaoSo}
-                onChange={(e) => setSoVaoSo(e.target.value)}
-                className={inputClassName()}
-              />
-            </div>
-            <div className="flex items-center gap-[30px]">
-              <label
-                htmlFor="HoTen"
-                className="w-[200px] text-right font-[500]"
-              >
-                Họ và tên
-              </label>
-              <input
-                type="text"
-                name="HoTen"
-                id="HoTen"
-                value={hoTen}
-                onChange={(e) => setHoTen(e.target.value)}
-                className={inputClassName()}
-              />
-            </div>
-            <div className="flex items-center gap-[30px]">
-              <label
-                htmlFor="MaXacNhan"
-                className="w-[200px] flex-shrink text-right font-[500]"
-              >
-                Mã xác nhận
-              </label>
-              <div className="flex flex-grow items-center gap-[30px]">
-                <input
-                  type="text"
-                  name="MaXacNhan"
-                  id="MaXacNhan"
-                  value={maXacNhan}
-                  onChange={(e) => setMaXacNhan(e.target.value)}
-                  className={inputClassName()}
-                />
-                <img
-                  className="flex h-[100%] w-[150px] rounded-md border border-[#5B6B79] object-cover"
-                  src="/images/captchaImage.png"
-                  alt="Captcha"
-                />
-              </div>
-            </div>
-          </div>
-          <Button
-            className="hover:bg-primary-0.9 cursor-pointer bg-[--primaryBackgroundColor] px-10 py-2"
-            type="submit"
-            asChild
+    <form
+      className="flex w-full flex-col items-center space-y-[20px] p-[10px]"
+      onSubmit={handleSubmit}
+      method="POST"
+    >
+      <div className="w-full space-y-[15px]">
+        <div className="flex items-center gap-[30px]">
+          <label
+            htmlFor="SoHieuVanBang"
+            className="w-[200px] text-right font-[500]"
           >
-            <span className="text-[20px]">Tra cứu</span>
-          </Button>
-        </form>
+            Số hiệu Văn bằng
+          </label>
+          <input
+            type="text"
+            name="SoHieuVanBang"
+            id="SoHieuVanBang"
+            value={soHieuVanBang}
+            onChange={(e) => setSoHieuVanBang(e.target.value)}
+            className={inputClassName()}
+          />
+        </div>
+        <div className="flex items-center gap-[30px]">
+          <label htmlFor="SoVaoSo" className="w-[200px] text-right font-[500]">
+            Số vào sổ
+          </label>
+          <input
+            type="text"
+            name="SoVaoSo"
+            id="SoVaoSo"
+            value={soVaoSo}
+            onChange={(e) => setSoVaoSo(e.target.value)}
+            className={inputClassName()}
+          />
+        </div>
+        <div className="flex items-center gap-[30px]">
+          <label htmlFor="HoTen" className="w-[200px] text-right font-[500]">
+            Họ và tên
+          </label>
+          <input
+            type="text"
+            name="HoTen"
+            id="HoTen"
+            value={hoTen}
+            onChange={(e) => setHoTen(e.target.value)}
+            className={inputClassName()}
+          />
+        </div>
+        <div className="flex items-center gap-[30px]">
+          <label
+            htmlFor="MaXacNhan"
+            className="w-[200px] flex-shrink text-right font-[500]"
+          >
+            Mã xác nhận
+          </label>
+          <div className="flex flex-grow items-center gap-[30px]">
+            <input
+              type="text"
+              name="MaXacNhan"
+              id="MaXacNhan"
+              value={maXacNhan}
+              onChange={(e) => setMaXacNhan(e.target.value)}
+              className={inputClassName()}
+            />
+            <img
+              className="flex h-[100%] w-[150px] rounded-md border border-[#5B6B79] object-cover"
+              src="/images/captchaImage.png"
+              alt="Captcha"
+            />
+          </div>
+        </div>
       </div>
-    </main>
+      <Button
+        className="cursor-pointer bg-[--primaryBackgroundColor] px-10 py-2 hover:bg-primary-0.9"
+        type="submit"
+        asChild
+      >
+        <span className="text-[20px]">Tra cứu</span>
+      </Button>
+    </form>
   );
 }
 
