e4429f0a6f21ecfbcbe0239fbfd01b86df7d2a5a
nhoden2k <nguyenvanbaom1515@gmail.com>
2024-05-02 22:40:04 +0700

    baoMerge

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import Home from "@/pages/user/Home";
-import DefaultLayoutUser from "@/layouts/user";
 import { Login, Home as AdminHome } from "@/pages/admin";
 import { adminLoginLayout, adminLayout } from "@/layouts/admin";
 import SearchByText from "@/pages/user/SearchByText";
@@ -17,4 +16,9 @@ const publicUserRoutes = [
   { path: "/result", component: Result, layout: RootLayoutUser },
 ];
 
-export { publicUserRoutes };
+const publicAdminRoutes = [
+  { path: "/", component: AdminHome, layout: adminLayout },
+  { path: "/login", component: Login, layout: adminLoginLayout },
+];
+
+export { publicUserRoutes, publicAdminRoutes };
